5cdbd4e7ba5834458288154b2620a509d73c4be8
Clark Cabillon <clarkcabillon7@gmail.com>
2026-02-10 18:45:17 +0800

    Add files via upload

---
+++ Cabillon_Web-Portfolio/script.js
@@ -0,0 +1,9 @@
+const sections = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    if (section.getBoundingClientRect().top < window.innerHeight - 100) {
+      section.classList.add('active');
+    }
+  });
+});
